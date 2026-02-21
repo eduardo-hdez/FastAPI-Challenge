ecc3d510aeacfe8406e2022559e24cdad7e4b189
eduardo-hdez <ehdez2004@gmail.com>
2026-02-20 21:18:11 -0600

    [Refactor] add sqlite imports and engine

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from app.database import engine, Base
+from app.models import item
 from app.routes import items
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(items.router)
 
 @app.get("/")
-async def root():
+def root():
     return RedirectResponse(url="/docs")
